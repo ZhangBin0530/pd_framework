b8706f9315e58616b6bdc82c86c6c0b2839b1845
ZhangBin0530 <45615524+ZhangBin0530@users.noreply.github.com>
2021-04-07 16:11:55 +0800

    Create main.py

--- example/main.py
+++ example/main.py
@@ -1,1 +1,12 @@
+from PD_flow import *
 
+pd_flow = PDFlow('flow.json')
+PyGetInputs = pd_flow.targets['PyGetInputs']
+PyGenCmds = pd_flow.targets['PyGenCmds']
+Floorplan = pd_flow.targets['Floorplan']
+Placement = pd_flow.targets['Placement']
+Cts = pd_flow.targets['Cts']
+Route = pd_flow.targets['Route']
+ChipFinish = pd_flow.targets['ChipFinish']
+run_list = [PyGetInputs,PyGenCmds,Floorplan,Placement,Cts,Route,ChipFinish]
+run(run_list)
